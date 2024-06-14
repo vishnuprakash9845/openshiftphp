a91e5de5505f445a85ffe9687dd37eaeffb268ce
vishnuprakash9845 <vishnuprakash9845@gmail.com>
2024-06-14 11:09:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body> 
 	<h1>Mayank MOdi</h1>
 		<p><b>Openshift Batch ABB Practical lab in AIC southfiled office  </b></p>
-		<p><b>Last day of training, checking automatic update</b></p>
+		<p><b>Last day of training, checking automatic update 12312456 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
